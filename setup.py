d47aa5313f33c339408ff1c21737c2eb88e1709b
marselshow <63909335+marselshow@users.noreply.github.com>
2020-04-18 15:32:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 NAME = "b0mb3r"
-DESCRIPTION = "Открытый и бесплатный СМС бомбер 💣"
-URL = "https://github.com/crinny/b0mb3r"
+DESCRIPTION = "lol 💣"
+URL = "https://github.com/marselshow/lol"
 EMAIL = ""
 AUTHOR = "crinny"
 REQUIRES_PYTHON = ">=3.7.0"
